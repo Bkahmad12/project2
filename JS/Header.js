bec89ef57c393798a8fb0b2b831fb5d29bcb6c7d
Bkahmad12 <Aptechbnr@gmail.com>
2022-06-03 11:19:59 -0700

    First commit

---
+++ JS/Header.js
@@ -0,0 +1,40 @@
+var header = document.getElementById('header').innerHTML` <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-dark" id="nav">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="Home.html">Home</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Categaries
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                        <li><a class="dropdown-item" href="#">Men's Wears</a></li>
+                        <li><a class="dropdown-item" href="#">Women's wers</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Kid's wears</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="Contact.html">Contact Us</a>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
+</div>`
